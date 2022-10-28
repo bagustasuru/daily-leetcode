5fe705b124a92e46f7988ab95d7f93187005496d
bagustasuru <bagustasuru97@gmail.com>
2022-10-28 11:31:51 +0700

    plus one

--- plus-one.js
+++ plus-one.js
@@ -11,11 +11,11 @@ var plusOne = function(digits) {
         firstIndex = 1;
     }
 
-    if (length <= 0 || length >= 9) {
+    if (length <= 0 || length >= 100) {
         return [];
     }
 
-    let result = String(Number(digits.join('')) + 1).split('');
+    let result = String(BigInt(digits.join('')) + BigInt(1)).split('');
     
 
     return result;
@@ -23,9 +23,13 @@ var plusOne = function(digits) {
 };
 
 console.log(plusOne(['1', '2', '3', '4', '5', '6', '7', '8', '9', '1', '2']));
+console.log(plusOne([9,8,7,6,5,4,3,2,1,0]));
 console.log(plusOne([]));
 console.log(plusOne(['1', '2', '3', '4']));
 console.log(plusOne(['9']));
 console.log(plusOne(['9','9']));
 console.log(plusOne(['0']));
 console.log(plusOne([0, 1, 4]));
+console.log(plusOne([9]));
+console.log(plusOne([1, 2, 3]));
+console.log(plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]));
