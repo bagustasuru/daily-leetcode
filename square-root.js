938587198fbc9f36595da0cf19a431013f4c99b4
bagustasuru <bagustasuru97@gmail.com>
2022-10-28 13:22:48 +0700

    square-root

---
+++ square-root.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+ var mySqrt = function(x) {
+    return Math.floor(x ** 0.5);
+};
+
+console.log(mySqrt(4));
+console.log(mySqrt(8));
+console.log(mySqrt(16));
